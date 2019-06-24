932a6233993755697a707ecae068c0b1fb12acc5
Ezequiel L. Aceto <ezequiel.aceto@gmail.com>
2019-06-24 08:10:30 -0300

    Add cocoapods example project

---
+++ Example/Pods/Target Support Files/Pods-Scudo_Example/Pods-Scudo_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Scudo_Example : NSObject
+@end
+@implementation PodsDummy_Pods_Scudo_Example
+@end
---
+++ Example/Pods/Target Support Files/Scudo/Scudo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Scudo : NSObject
+@end
+@implementation PodsDummy_Scudo
+@end
